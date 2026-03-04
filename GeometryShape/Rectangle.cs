754c0beb8623df4d671eb9af91aa1a700d944c84
unknown <you@example.com>
2026-03-04 15:43:45 +0500

    first

---
+++ GeometryShape/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public override double Area()
+        {
+            try
+            {
+                if (CorrectValues())
+                    return Width * Height;
+                else
+                    throw new ArgumentException("Стороны не могут быть меньше либо равны нулю");
+            }
+            catch
+            {
+                throw new ArgumentException("Некорректные данные");
+            }
+        }
+        public override double Perimeter()
+        {
+            try
+            {
+                if (CorrectValues())
+                    return 2 * (Width + Height);
+                else 
+                    throw new ArgumentException("Стороны не могут быть меньше либо равны нулю");
+            }
+            catch
+            {
+                throw new ArgumentException("Некорректные данные");
+            }
+        }
+        public override string ToString()
+        {
+            return $"Ширина = {Width} Высота = {Height}";
+        }
+        private bool CorrectValues()
+        {
+            if (Width <= 0 || Height <= 0)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ GeometryShape/Triangle.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryShape
+{
+    public class Triangle: Shape
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+        public override double Area()
+        {
+            try
+            {
+                if (CorrectValues())
+                {
+                    if (TriangleExist())
+                    {
+                        double p = Perimeter() / 2;
+                        return Math.Sqrt(p * (p - SideA) * (p - SideB) * (p - SideC));
+                    }
+                    else
+                        throw new ArgumentException("Такого треугольник не существует. Неправильно введены стороны");
+                }
+                else
+                    throw new ArgumentException("Стороны не могут быть меньше либо равны нулю");
+            }
+            catch
+            {
+                throw new ArgumentException("Некорректные данные");
+            }
+        }
+        public override double Perimeter()
+        {
+            try
+            {
+                if (CorrectValues())
+                {
+                    if (TriangleExist())
+                        return SideA + SideB + SideC;
+                    else
+                        throw new ArgumentException("Такого треугольник не существует. Неправильно введены стороны");
+                }
+                else
+                    throw new ArgumentException("Стороны не могут быть меньше либо равны нулю");
+            }
+            catch
+            {
+                throw new ArgumentException("Некорректные данные");
+            }
+        }
+        public override string ToString()
+        {
+            return $"Сторона А = {SideA} Сторона В = {SideB} Сторона С = {SideC}";
+        }
+        private bool TriangleExist()
+        {
+            if ((SideA + SideB > SideC) && (SideA + SideC > SideB) && (SideB + SideC > SideA))
+                return true;
+            return false;
+        }
+        private bool CorrectValues()
+        {
+            if (SideA <= 0 || SideB <= 0 || SideC <=0)
+                return false;
+            return true;
+        }
+    }
+}
